8eea5247a54a2fdffd476b82ae30e6833f298fb9
André Riedel <andreriedelz@gmail.com>
2022-10-06 18:27:21 -0300

    add statements

---
+++ 4-statements/statements.js
@@ -0,0 +1,14 @@
+let a, b, c; // statement 1
+a = 5; // statement 2
+b = 6; // statement 3
+c = a + b; // statement 4
+
+console.log(c);
+
+// statements can be grouped
+
+block : {
+  let x = 10;
+  console.log(x);
+  console.log("Hello World");
+}
