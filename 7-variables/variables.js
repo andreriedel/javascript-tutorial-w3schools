4c1d1287a2f92a60e72b75f59fdd54f97671354e
André Riedel <andreriedelz@gmail.com>
2022-10-06 19:19:01 -0300

    add variables

---
+++ 7-variables/variables.js
@@ -0,0 +1,23 @@
+// declared variable with var
+var x = 1;
+
+// declared variable with let
+let y = 2;
+
+// undeclared variable
+z = 3;
+
+// declared const
+const w = 4;
+
+// declared varibale with undefined type; not initialized
+let name;
+
+// multiple variables
+let person = "John Doe", carName = "Volvo", price = 200;
+
+// You cannot re-declare a variable declared with let or const.
+/*
+  let carName = "Volvo";
+  let carName;
+*/
