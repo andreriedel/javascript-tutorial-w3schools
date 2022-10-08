10f3db202bd2b1fee6ea8d6a598809cb27a3b82c
André Riedel <andreriedelz@gmail.com>
2022-10-07 21:52:39 -0300

    add operators

---
+++ 10-operators/operators.js
@@ -0,0 +1,134 @@
+/* -------------------------------------------------------------------------- */
+/*                            arithmetic operators                            */
+/* -------------------------------------------------------------------------- */
+
+let x = 10;
+
+console.log(x + 5);
+console.log(x - 5);
+console.log(x * 5);
+console.log(x / 5);
+console.log(x ** 2);
+console.log(x % 3);
+console.log(++x);
+console.log(--x);
+
+console.log('\n');
+
+/* -------------------------------------------------------------------------- */
+/*                            assignment operators                            */
+/* -------------------------------------------------------------------------- */
+
+let a = 6;
+console.log(a += 3);
+
+let b = 6;
+console.log(b -= 3);
+
+let c = 6;
+console.log(c *= 3);
+
+let d = 6;
+console.log(d /= 3);
+
+let e = 6;
+console.log(e %= 3);
+
+let f = 6;
+console.log(f **= 3);
+
+console.log('\n');
+
+/* -------------------------------------------------------------------------- */
+/*                              string operators                              */
+/* -------------------------------------------------------------------------- */
+
+let firstName = "Andre";
+let lastName = "Zambroni";
+let fullName = firstName + " " + lastName;
+
+fullName += " " + "Riedel";
+
+console.log(fullName);
+
+/* -------------------------------------------------------------------------- */
+
+let str1 = 5 + 5;
+let str2 = 5 + 5 + '5'; // do the sum and then concatenate
+let str3 = '5' + 5;
+let str4 = "Hello" + 5;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+
+console.log('\n');
+
+/* -------------------------------------------------------------------------- */
+/*                               type operators                               */
+/* -------------------------------------------------------------------------- */
+
+let array = ["larissa", "andre", "andressa"];
+
+console.log(typeof array);
+console.log(array instanceof Array);
+
+console.log('\n');
+
+/* -------------------------------------------------------------------------- */
+/*                              bitwise operators                             */
+/* -------------------------------------------------------------------------- */
+
+/* --------------------------------- and (&) -------------------------------- */
+
+let and1 = 0b00100110; // número 38 em binário
+let and2 = 0b00101000; // número 40 em binário
+//           --------
+//           00100000     número 32 em binário
+
+console.log(and1 & and2);
+
+/* --------------------------------- or (|) --------------------------------- */
+
+let or1 = 0b00100110; // número 38 em binário
+let or2 = 0b00101000; // número 40 em binário
+//          --------
+//          00101110     número 46 em binário
+
+console.log(or1 | or2);
+
+/* --------------------------------- xor (^) -------------------------------- */
+
+let xor1 = 0b00100110; // número 38 em binário
+let xor2 = 0b00101000; // número 40 em binário
+//           --------
+//           00001110     número 14 em binário
+
+console.log(xor1 ^ xor2);
+
+/* --------------------------------- not (~) -------------------------------- */
+
+let num = 5; // em binário (32 bits): 00000000000000000000000000000101
+let notNum = ~num; // em binário (32 bits): 11111111111111111111111111111010
+
+/* como os números em JavaScript são sempre assinalados (signed), o resultado
+retornará -6; caso fosse unsigned o resultado seria 4294967290 */
+
+console.log(notNum);
+
+/* ----------------------------- left shift (<<) ---------------------------- */
+
+let lf = 0b0101; // número 5 em binário
+//         ----
+//         1010     número 10 em binário
+
+console.log(lf << 1);
+
+/* ---------------------------- right shift (>>) ---------------------------- */
+
+let rs = 0b0101; // número 5 em binário
+//         ----
+//         0010     número 2 em binário
+
+console.log(lf >> 1);
