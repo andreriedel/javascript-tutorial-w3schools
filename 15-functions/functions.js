33770f817e79b37ddd25de51ad9836994773efdf
André Riedel <andreriedelz@gmail.com>
2022-10-08 16:24:44 -0300

    add functions

---
+++ 15-functions/functions.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(10, 5));
+
+const minus = function (a, b) {
+  return a - b;
+}
+
+console.log(minus(10, 5));
+
+const times = (a, b) => a * b;
+
+console.log(times(10, 5));
