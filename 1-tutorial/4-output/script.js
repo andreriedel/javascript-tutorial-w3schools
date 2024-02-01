a0051308fdb11054227ed10f01fe4bf6a85f875f
André Riedel <andreriedelz@gmail.com>
2024-01-31 21:50:51 -0300

    feat(tutorial): add output

---
+++ 1-tutorial/4-output/script.js
@@ -0,0 +1,13 @@
+document.getElementById("demo").innerHTML = "innerHTML";
+
+document.write("document.write");
+
+window.alert("window.alert");
+
+console.log("console.log");
+
+function myFunction(){
+    window.print();
+}
+
+// o uso do objeto window é opcional, pois ele é o objeto de escopo global
