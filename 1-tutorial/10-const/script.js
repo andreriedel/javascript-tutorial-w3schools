72c335c24a858d40d8c79254a1f3c6ef88b756a3
André Riedel <andreriedelz@gmail.com>
2024-02-02 19:38:00 -0300

    feat(tutorial): add const

---
+++ 1-tutorial/10-const/script.js
@@ -0,0 +1,28 @@
+// a const variable cannot be reassigned
+// const variables must be initialized when declared
+
+const PI = 3.14159265359;
+
+// PI = 3.14; error
+
+/* -------------------------------------------------------------------------- */
+
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// You can change an element:
+cars[0] = "Toyota";
+
+// You can add an element:
+cars.push("Audi");
+
+/* -------------------------------------------------------------------------- */
+
+// You can create a const object:
+const car = {type:"Fiat", model:"500", color:"white"};
+
+// You can change a property:
+car.color = "red";
+
+// You can add a property:
+car.owner = "Johnson";
