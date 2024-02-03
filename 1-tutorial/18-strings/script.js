3976061429ea6ba65104cf0379336c944a74174e
André Riedel <andreriedelz@gmail.com>
2024-02-03 17:06:06 -0300

    feat(tutorial): add strings

---
+++ 1-tutorial/18-strings/script.js
@@ -0,0 +1,28 @@
+console.log('Deserunt amet eu ex "aliquip" et non est eu voluptate velit cillum fugiat.');
+console.log("Deserunt amet eu ex 'aliquip' et non est eu voluptate velit cillum fugiat.");
+console.log("Deserunt amet eu ex \"aliquip\" et non est eu voluptate velit cillum fugiat.");
+console.log('Deserunt amet eu ex \'aliquip\' et non est eu voluptate velit cillum fugiat.');
+console.log('Deserunt amet eu ex \\ aliquip et non est eu voluptate velit cillum fugiat.');
+
+let text = `He's often called "Johnny"`; // template string
+console.log(text);
+
+let x = "John"; // primitive value
+let y = new String("John"); // object; not recommended
+
+console.log(typeof x);
+console.log(typeof y);
+
+console.log(x == y);
+console.log(x === y);
+
+/* -------------------------------------------------------------------------- */
+
+/*
+  \b    Backspace
+  \f    Form Feed
+  \n    New Line
+  \r    Carriage Return
+  \t    Horizontal Tabulator
+  \v    Vertical Tabulator
+*/
