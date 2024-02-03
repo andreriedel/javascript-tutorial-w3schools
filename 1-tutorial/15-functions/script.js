ab6d43fb1253de395203a32bf2536d687a5f4141
André Riedel <andreriedelz@gmail.com>
2024-02-02 21:07:21 -0300

    feat(tutorial): add functions

---
+++ 1-tutorial/15-functions/script.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(10, 5));
+
+const minus = function (a, b) {
+  return a - b;
+}
+
+console.log(minus(10, 5));
+
+const times = (a, b) => a * b;
+
+console.log(times(10, 5));
+
+/*
+  Function parameters are listed inside the parentheses () in the function definition.
+  Function arguments are the values received by the function when it is invoked.
+*/
